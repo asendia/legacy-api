e38e513e8a4649d9fe12a8bd98020b3365de54bb
Asendia Mayco <asendia@traveloka.com>
2022-02-16 04:00:58 +0700

    Convert reminder-api to pub/sub format

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -14,7 +16,18 @@ func main() {
 	port := 8080
 	http.HandleFunc("/legacy-api", p.CloudFunctionForFrontendWithNetlifyJWT)
 	http.HandleFunc("/legacy-api-secret", p.CloudFunctionForFrontendWithUserSecret)
-	http.HandleFunc("/legacy-api-scheduler", p.CloudFunctionForSchedulerWithStaticSecret)
+	http.HandleFunc("/legacy-api-scheduler", handleScheduler)
 	log.Printf("Server is running on localhost:%d", port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
+}
+
+func handleScheduler(w http.ResponseWriter, r *http.Request) {
+	var psm p.PubSubMessage
+	json.NewDecoder(r.Body).Decode(&psm)
+	err := p.CloudFunctionForSchedulerWithStaticSecret(r.Context(), psm)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %+v\n", err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Success!")
 }
