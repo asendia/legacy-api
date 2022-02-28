1c9dbaf06d047986528dc43243cf8574f5a6847d
Asendia Mayco <asendia@traveloka.com>
2022-03-01 02:05:10 +0700

    Add cloudbuild

--- data/schema_test.sql
+++ data/schema_test.sql
@@ -1,5 +1,5 @@
-ALTER TABLE public.emails OWNER TO project_legacy_admin;
+ALTER TABLE public.emails OWNER TO project_legacy_tester;
 
-ALTER TABLE public.messages OWNER TO project_legacy_admin;
+ALTER TABLE public.messages OWNER TO project_legacy_tester;
 
-ALTER TABLE public.messages_email_receivers OWNER TO project_legacy_admin;
+ALTER TABLE public.messages_email_receivers OWNER TO project_legacy_tester;
